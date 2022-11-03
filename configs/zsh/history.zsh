56159be50271848cdc4cbe2e78aef6b8933d7651
TcPhoenix2000 <tibo.coppens@outlook.com>
2022-11-03 16:31:03 +0100

    adding zshrc , zsh_history , p10k files

---
+++ configs/zsh/history.zsh
@@ -0,0 +1,354 @@
+: 1666196486:0;nano ~/.zshrc
+: 1666197036:0;cd ./challenges-linux-essentials-TcPhoenix2000 
+: 1666197162:0;clear
+: 1666197643:0;exec zsh
+: 1666197652:0;exec bash
+: 1666334755:0;chsh -s /bin/bash
+: 1666334764:0;exec bash
+: 1666341036:0;cd
+: 1666341056:0;nano ~/.zshrc
+: 1666341132:0;cd
+: 1666341263:0;sudo apt install neofetch
+: 1666341282:0;clear
+: 1666341750:0;cd
+: 1666341774:0;ls
+: 1666341779:0;cd ./dotfiles
+: 1666341787:0;git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k\
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+: 1666341829:0;nano ~/.zshrc
+: 1666342135:0;cd
+: 1666348468:0;ls -l -- ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+: 1666348557:0;cd 
+: 1666348563:0;ls -al
+: 1666348726:0;git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+: 1666348766:0;rm -rf powerlevel10k 
+: 1666348811:0;ls -al
+: 1666348951:0;cp .p10k.zsh .oh-my-zsh/custom/
+: 1666348997:0;ls -al .oh-my-zsh/custom/themes
+: 1666349078:0;git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k\
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+: 1666349096:0;ls -al 
+: 1666366844:0;clear
+: 1666366853:0;cat sources.list
+: 1666366860:0;find  sources.list
+: 1666366878:0;locate  sources.list
+: 1666366910:0;cat /etc/apt/sources.list
+: 1666367160:0;clear
+: 1666774778:0;cd ./projects/les-26-10/snake
+: 1666774782:0;make
+: 1666774820:0;./bin/program
+: 1666774851:0;make clean
+: 1666774859:0;clear
+: 1666775433:0;git init
+: 1666775483:0;git clone git@github.com:vives-oop2-2022-2023/snake-challenge-TcPhoenix2000.git
+: 1666775556:0;cd ../
+: 1666775560:0;git clone git@github.com:vives-oop2-2022-2023/snake-challenge-TcPhoenix2000.git
+: 1666775935:0;cd ./projects/les-26-10/snake-challenge-TcPhoenix2000
+: 1666783965:0;git status
+: 1666789578:0;cd ./projects/les-26-10/snake-challenge-TcPhoenix2000
+: 1666789586:0;git status
+: 1666801521:0;git add .
+: 1666801571:0;git commit -m "first commit"
+: 1666801610:0;git push origin master
+: 1666802350:0;git status
+: 1666802370:0;cd ./challenges-linux-essentials-TcPhoenix2000
+: 1666802372:0;git status
+: 1666802385:0;git add .
+: 1666802480:0;git commit -m "modified chapter c_1 till tmp filesystem"
+: 1666802502:0;git push origin master
+: 1666802656:0;cd ./http-Unit-converter
+: 1666802663:0;git status
+: 1666803482:0;git add.
+: 1666803488:0;git add .
+: 1666803509:0;git commit -m "first commit"
+: 1666803569:0;git push origin master
+: 1666803585:0;git push origin main
+: 1666803663:0;cd ../
+: 1666803671:0;git clone git@github.com:vives-webscripting-2022/unit-converter-TcPhoenix2000.git
+: 1666803772:0;cd ./unit-converter-TcPhoenix2000
+: 1666803797:0;git status
+: 1666803816:0;git add .
+: 1666803822:0;git commit -m "first commit"
+: 1666803825:0;git status
+: 1666803850:0;git push origin main
+: 1666944949:0;clear
+: 1666945027:0;cd ~
+: 1666945069:0;touch first-of-many
+: 1666945081:0;nano first-of-many
+: 1666945132:0;ls -al
+: 1666945183:0;ls -al first-of-many
+: 1666945565:0;touch second try
+: 1666945570:0;ls
+: 1666945587:0;ls -al
+: 1667126034:0;clear
+: 1667126045:0;cd ./linux-challenges/challenges-linux-essentials-TcPhoenix2000
+: 1667126054:0;git status
+: 1667126073:0;cd /
+: 1667126078:0;ls
+: 1667126122:0;mkdir backups
+: 1667126235:0;sudo mkdir backups
+: 1667126250:0;cd ./backups
+: 1667126271:0;touch README.md
+: 1667126495:0;cd ~
+: 1667126543:0;cp .bashrc .bashrc.bak
+: 1667126550:0;ls
+: 1667126555:0;ls -al
+: 1667126918:0;man ln
+: 1667314379:0;clear
+: 1667314434:0;dc
+: 1667314437:0;cd
+: 1667314442:0;ls -al
+: 1667314521:0;tree dotfiles
+: 1667315213:0;ls -al
+: 1667315548:0;cat first-of-many
+: 1667315558:0;rm first-of-many
+: 1667315566:0;cat second
+: 1667315573:0;rm second
+: 1667315582:0;rm try
+: 1667315585:0;ls -al
+: 1667315686:0;cat .pub
+: 1667315690:0;ls -al
+: 1667316163:0;cd
+: 1667316169:0;ls -al
+: 1667316222:0;touch secrets
+: 1667316244:0;ln secrets /etc/passwd
+: 1667316266:0;sudo ln secrets /etc/passwd
+: 1667316277:0;rm secrets
+: 1667316280:0;ln secrets /etc/passwd
+: 1667316297:0;mkdir secrets
+: 1667316299:0;ln secrets /etc/passwd
+: 1667316329:0;ls -al
+: 1667316349:0;rm secrets
+: 1667316359:0;rm -f secrets
+: 1667316394:0;mv secrets  /tmp/
+: 1667316400:0;ls -al
+: 1667316478:0;ln secrets /etc/passwd
+: 1667316495:0;Ln secrets /etc/passwd
+: 1667316507:0;sudo ln secrets /etc/passwd
+: 1667316528:0;touch secrets
+: 1667316531:0;Ln secrets /etc/passwd
+: 1667316536:0;ln secrets /etc/passwd
+: 1667316553:0;cat secrets
+: 1667319709:0;clear
+: 1667320615:0;git status
+: 1667321837:0;git add .
+: 1667321938:0;git commit -m "modified chapter c_1 till sym linking"
+: 1667321966:0;git push origin master
+: 1667391497:0;clear
+: 1667393649:0;cd ./les-26-10/snake-challenge-TcPhoenix2000
+: 1667395113:0;make
+: 1667395283:0;make clean
+: 1667395288:0;make
+: 1667395511:0;./bin/app
+: 1667395602:0;make
+: 1667395650:0;make clean
+: 1667395682:0;make
+: 1667395889:0;./bin/program
+: 1667395957:0;make clean
+: 1667395962:0;make
+: 1667395965:0;./bin/program
+: 1667396022:0;make
+: 1667396024:0;./bin/program
+: 1667396082:0;make
+: 1667396091:0;./bin/program
+: 1667396483:0;make
+: 1667396492:0;./bin/program
+: 1667396537:0;make
+: 1667396605:0;./bin/program
+: 1667396640:0;make
+: 1667396643:0;./bin/program
+: 1667396824:0;make
+: 1667396826:0;./bin/program
+: 1667396879:0;make
+: 1667396881:0;./bin/program
+: 1667396919:0;make
+: 1667396921:0;./bin/program
+: 1667396987:0;make
+: 1667396989:0;./bin/program
+: 1667397065:0;make
+: 1667397066:0;./bin/program
+: 1667397103:0;make
+: 1667397105:0;./bin/program
+: 1667397329:0;make
+: 1667397331:0;./bin/program
+: 1667397350:0;make
+: 1667397353:0;./bin/program
+: 1667397370:0;make
+: 1667397375:0;./bin/program
+: 1667397766:0;make
+: 1667397768:0;./bin/program
+: 1667397791:0;make
+: 1667397793:0;./bin/program
+: 1667397836:0;make
+: 1667397845:0;./bin/program
+: 1667397855:0;make
+: 1667397858:0;./bin/program
+: 1667397902:0;make
+: 1667397906:0;./bin/program
+: 1667397929:0;make
+: 1667397930:0;./bin/program
+: 1667397964:0;make
+: 1667397966:0;./bin/program
+: 1667399490:0;make
+: 1667399821:0;./bin/program
+: 1667400047:0;make
+: 1667400048:0;./bin/program
+: 1667400078:0;make
+: 1667400080:0;./bin/program
+: 1667400091:0;make
+: 1667400098:0;./bin/program
+: 1667400146:0;make
+: 1667400149:0;./bin/program
+: 1667400161:0;make
+: 1667400164:0;./bin/program
+: 1667400229:0;make
+: 1667400231:0;./bin/program
+: 1667400281:0;make
+: 1667400284:0;./bin/program
+: 1667400320:0;make
+: 1667400323:0;./bin/program
+: 1667400354:0;make
+: 1667400356:0;./bin/program
+: 1667400379:0;make
+: 1667400381:0;./bin/program
+: 1667400411:0;make
+: 1667400413:0;./bin/program
+: 1667400451:0;make
+: 1667400453:0;./bin/program
+: 1667400470:0;make
+: 1667400472:0;./bin/program
+: 1667400503:0;make
+: 1667400506:0;./bin/program
+: 1667400516:0;make
+: 1667400521:0;./bin/program
+: 1667400539:0;make
+: 1667400541:0;./bin/program
+: 1667400558:0;make
+: 1667400561:0;./bin/program
+: 1667400710:0;make
+: 1667400712:0;./bin/program
+: 1667400743:0;make
+: 1667400745:0;./bin/program
+: 1667403233:0;make
+: 1667403236:0;./bin/program
+: 1667403476:0;make
+: 1667403478:0;./bin/program
+: 1667403496:0;make
+: 1667403498:0;./bin/program
+: 1667403616:0;make
+: 1667403618:0;./bin/program
+: 1667403757:0;make
+: 1667403759:0;./bin/program
+: 1667403813:0;make
+: 1667403815:0;./bin/program
+: 1667403844:0;make
+: 1667403846:0;./bin/program
+: 1667403926:0;make
+: 1667403928:0;./bin/program
+: 1667403954:0;make
+: 1667403956:0;./bin/program
+: 1667404246:0;make
+: 1667404248:0;./bin/program
+: 1667404291:0;make
+: 1667404293:0;./bin/program
+: 1667404855:0;make
+: 1667405713:0;make clean
+: 1667405825:0;make
+: 1667408544:0;./bin/program
+: 1667408684:0;make
+: 1667409008:0;make clean
+: 1667409011:0;make
+: 1667409232:0;./bin/program
+: 1667409262:0;make clean
+: 1667409321:0;make
+: 1667409333:0;./bin/program
+: 1667409473:0;make
+: 1667409524:0;./bin/program
+: 1667409553:0;make
+: 1667409556:0;./bin/program
+: 1667409579:0;make
+: 1667409582:0;./bin/program
+: 1667409646:0;make
+: 1667409648:0;./bin/program
+: 1667409763:0;make
+: 1667409765:0;./bin/program
+: 1667409786:0;make
+: 1667409788:0;./bin/program
+: 1667409812:0;git add .
+: 1667409978:0;git commit -m "first succesfull print of 2d array with borders to use in the canvas.cpp "
+: 1667409997:0;git push origin master
+: 1667410178:0;make clean
+: 1667410186:0;git add .
+: 1667410217:0;git commit -m "removed temp .o filles "
+: 1667410225:0;git push origin master
+: 1667410855:0;make 
+: 1667410952:0;make clean
+: 1667411265:0;make 
+: 1667411274:0;./bin/program
+: 1667411316:0;cd ./mnt/d/E/j/o/projects/les-26-10/snake-challenge-TcPhoenix2000
+: 1667411363:0;cd ./mntcccccc/////c/c/cG
+: 1667411415:0;cd ./challenges-linux-essentials-TcPhoenix2000
+: 1667411424:0;ls
+: 1667411467:0;cd "D:\Electronica -ICT\jaar_2-semester_1\object-oriented-programing\projects\les-26-10\snake-challenge-TcPhoenix2000"
+: 1667411480:0;cd D:\Electronica -ICT\jaar_2-semester_1\object-oriented-programing\projects\les-26-10\snake-challenge-TcPhoenix2000
+: 1667411518:0;cd /mnt/D:\Electronica -ICT\jaar_2-semester_1\object-oriented-programing\projects\les-26-10\snake-challenge-TcPhoenix2000
+: 1667411534:0;cd /mnt/d
+: 1667411574:0;cd /mnt/d/Electronica\ -ICT/jaar_2-semester_1/object-oriented-programing/projects/les-26-10/snake-challenge-TcPhoenix2000
+: 1667411587:0;./bin/program
+: 1667411638:0;make 
+: 1667411643:0;./bin/program
+: 1667411700:0;make 
+: 1667411705:0;./bin/program
+: 1667411733:0;make 
+: 1667411737:0;./bin/program
+: 1667411769:0;make 
+: 1667411772:0;./bin/program
+: 1667412129:0;make 
+: 1667412284:0;./bin/program
+: 1667413183:0;make 
+: 1667413186:0;./bin/program
+: 1667413332:0;make 
+: 1667413342:0;./bin/program
+: 1667413625:0;make 
+: 1667413628:0;./bin/program
+: 1667413769:0;make 
+: 1667413873:0;./bin/program
+: 1667413933:0;make 
+: 1667413935:0;./bin/program
+: 1667413942:0;make clean
+: 1667414462:0;make 
+: 1667414468:0;make clean
+: 1667414472:0;make 
+: 1667414481:0;./bin/program
+: 1667414488:0;make clean
+: 1667414583:0;make 
+: 1667414589:0;./bin/program
+: 1667419031:0;make clean
+: 1667419094:0;clear
+: 1667487060:0;cd
+: 1667487066:0;ls -al
+: 1667487298:0;cd dotfiles
+: 1667487303:0;ls -al
+: 1667487315:0;tree configs
+: 1667487346:0;cd /configs
+: 1667487360:0;cd configs
+: 1667487392:0;mkdir zsh
+: 1667487399:0;cd ../
+: 1667487490:0;cp .zshrc dotfiles/configs/zsh
+: 1667487510:0;cat dotfiles/configs/zsh/
+: 1667487520:0;cat dotfiles/configs/zsh/.zshrc
+: 1667487598:0;cd dotfiles
+: 1667487622:0;tree dotfiles/configs/
+: 1667487626:0;tree dotfiles/configs
+: 1667487633:0;tree  configs
+: 1667487811:0;cd configs
+: 1667487818:0;cd zsh
+: 1667487824:0;ls -al
+: 1667487857:0;cp .zshrc zshrc
+: 1667487865:0;ls -al
+: 1667487877:0;rm .zshrc
+: 1667487883:0;ls -al
+: 1667487949:0;cd ../../../
+: 1667487953:0;ls -al
+: 1667488235:0;cp .p10k.zsh dotfiles/configs/zsh/p10k.zsh
+: 1667488315:0;cp .zsh_history dotfiles/configs/zsh/history.zsh
